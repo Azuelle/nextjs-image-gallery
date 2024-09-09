acbd5c46bd6c12d913e66856cb976e3f14deb343
夢野 雫 <tjk@live.cn>
2024-09-09 17:35:12 +0800

    Fix next.config.js

--- next.config.js
+++ next.config.js
@@ -1,13 +1,12 @@
 module.exports = {
   images: {
-    unoptimized: true,
     formats: ["image/avif", "image/webp"],
     remotePatterns: [
       {
         protocol: "https",
         hostname: "res.cloudinary.com",
         port: "",
-        pathname: "/my-account/**",
+        pathname: "/azuelle/**",
       },
     ],
   },
